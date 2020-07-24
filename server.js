57528eb9251b79f123d0fe8afa67157f77061cff
Mohanraj Chandrasekar <mohanraj.c@mpro.co>
2020-07-24 16:52:55 +0530

    added.

--- server.js
+++ server.js
@@ -6,30 +6,47 @@ var fs = require("fs");
 var runner = require("child_process");
 var phpScriptPath = "./script.php";
 const lti = './lti_consumer.php';
-var argsString = "value1,value2,value3";
+// var argsString = "value1,value2,value3";
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
 app.use('/', router);
 
-router.post('/launch', function (req, res) {
-   // console.l+og(req.params);
-   // const data = req.params;
-   console.log(req.body);
-   const object = Object.assign({}, req.body);
-   console.log(object);
-   const launchURL = 'http://udapqa.southindia.cloudapp.azure.com/udapdev/ltiDeliveryProvider/DeliveryTool/launch/eyJkZWxpdmVyeSI6Imh0dHA6XC9cL3VkYXBxYS5zb3V0aGluZGlhLmNsb3VkYXBwLmF6dXJlLmNvbVwvdWRhcGRldlwvdWRhcC5yZGYjaTE1OTQyMzE4ODAyMTgxNjQifQ==';
-   const secret = 'LTIConsumer123456789';
-   const sharedSecret = 'LTIConsumer123456789s';
-
-   var argsString = launchURL + ',' + secret + ',' + sharedSecret;
-
-runner.exec("php " + lti + ' ' + argsString, function(err, phpResponse, stderr) {
-    if(err) console.log(err); /* log error */
-   // console.log( phpResponse );
-   res.send(phpResponse);
-   });
+router.get('/get', function(req, res) {
+   res.send('Get up...');
 });
+
+router.post('/launch', function (req, res) {
+   try {
+      const object = Object.assign({}, req.body);
+      console.log(object);
+      // const launchURL = 'http://udapqa.southindia.cloudapp.azure.com/udapdev/ltiDeliveryProvider/DeliveryTool/launch/eyJkZWxpdmVyeSI6Imh0dHA6XC9cL3VkYXBxYS5zb3V0aGluZGlhLmNsb3VkYXBwLmF6dXJlLmNvbVwvdWRhcGRldlwvdWRhcC5yZGYjaTE1OTQyMzE4ODAyMTgxNjQifQ==';
+      // const secret = 'LTIConsumer123456789';
+      // const sharedSecret = 'LTIConsumer123456789s';
+
+      var argsString = object.launchURL + ',' + object.secret + ',' + object.sharedSecret;
+      runner.exec("php " + lti + ' ' + argsString, function (err, phpResponse, stderr) {
+         if (err) {
+            console.log(err); /* log error */
+            processResponse(false, 500, 'Error While Fetching Data!', err, res);
+         } else {
+            processResponse(true, 200, 'Successfully Fetched!', phpResponse, res);
+         }
+      });
+   } catch (err) {
+      console.log(err);
+      processResponse(false, 500, 'Error While Fetching Data!', err, res);
+   }
+});
+
+async function processResponse(statusBool, statusHttp, msg, result, response) {
+   response.send({
+      statusBool: statusBool,
+      statusHttp: statusHttp,
+      message: msg,
+      data: result
+   });
+}
 
 var server = app.listen(8081, function () {
    var host = server.address().address;
